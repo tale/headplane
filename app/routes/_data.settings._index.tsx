224621d9a5910745c3ae68a6fa14e153b1de5cda
Aarnav Tale <aarnavtale@icloud.com>
2024-03-30 04:47:46 -0400

    fix: actions needs ghcr.io

--- app/routes/_data.settings._index.tsx
+++ app/routes/_data.settings._index.tsx
@@ -1,18 +1,13 @@
-import { useLoaderData } from '@remix-run/react'
-
-import { getConfig } from '~/utils/config'
-
-export async function loader() {
-	const config = await getConfig()
-	return config
-}
+import { CubeTransparentIcon } from '@heroicons/react/24/outline'
 
 export default function Page() {
-	const data = useLoaderData<typeof loader>()
-
 	return (
-		<div>
-			{JSON.stringify(data, null, 4)}
+		<div className='w-96 mx-auto flex flex-col justify-center items-center text-center'>
+			<CubeTransparentIcon className='w-32 h-32 text-gray-500'/>
+			<p className='text-lg mt-8'>
+				The settings page is currently unavailable.
+				It will be available in a future release.
+			</p>
 		</div>
 	)
 }
