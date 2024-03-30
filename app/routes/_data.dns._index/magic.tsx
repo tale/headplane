b146e4c3a89e548d18d5ca6581620889ea93a108
Aarnav Tale <aarnavtale@icloud.com>
2024-03-30 18:39:46 -0400

    feat: add confirmation modal

--- app/routes/_data.dns._index/magic.tsx
+++ app/routes/_data.dns._index/magic.tsx
@@ -1,9 +1,7 @@
-import { Dialog } from '@headlessui/react'
 import { useFetcher } from '@remix-run/react'
-import clsx from 'clsx'
-import { useState } from 'react'
 
 import Button from '~/components/Button'
+import useModal from '~/components/Modal'
 import Spinner from '~/components/Spinner'
 
 type Properties = {
@@ -13,8 +11,22 @@ type Properties = {
 }
 
 export default function Modal({ isEnabled, disabled }: Properties) {
-	const [isOpen, setIsOpen] = useState(false)
 	const fetcher = useFetcher()
+	const { Modal, open } = useModal({
+		title: `${isEnabled ? 'Disable' : 'Enable'} Magic DNS`,
+		variant: isEnabled ? 'danger' : 'confirm',
+		buttonText: `${isEnabled ? 'Disable' : 'Enable'} Magic DNS`,
+		description: 'Devices will no longer be accessible via your tailnet domain. The search domain will also be disabled.',
+		onConfirm: () => {
+			fetcher.submit({
+				// eslint-disable-next-line @typescript-eslint/naming-convention
+				'dns_config.magic_dns': !isEnabled
+			}, {
+				method: 'PATCH',
+				encType: 'application/json'
+			})
+		}
+	})
 
 	return (
 		<>
@@ -23,7 +35,7 @@ export default function Modal({ isEnabled, disabled }: Properties) {
 				className='w-fit text-sm'
 				disabled={disabled}
 				onClick={() => {
-					setIsOpen(true)
+					open()
 				}}
 			>
 				{fetcher.state === 'idle' ? undefined : (
@@ -31,46 +43,7 @@ export default function Modal({ isEnabled, disabled }: Properties) {
 				)}
 				{isEnabled ? 'Disable' : 'Enable'} Magic DNS
 			</Button>
-			<Dialog
-				className='relative z-50'
-				open={isOpen} onClose={() => {
-					setIsOpen(false)
-				}}
-			>
-				<div className='fixed inset-0 bg-black/30' aria-hidden='true'/>
-				<div className='fixed inset-0 flex w-screen items-center justify-center'>
-					<Dialog.Panel className='bg-white rounded-lg p-4 w-full max-w-md'>
-						<Dialog.Title className='text-lg font-bold'>
-							{isEnabled ? 'Disable' : 'Enable'} Magic DNS
-						</Dialog.Title>
-						<Dialog.Description className='text-gray-500 dark:text-gray-400'>
-							Devices will no longer be accessible via your tailnet domain.
-							The search domain will also be disabled.
-						</Dialog.Description>
-						<Button
-							variant='emphasized'
-							type='submit'
-							className={clsx(
-								'w-full mt-12',
-								isEnabled ? 'bg-red-800 dark:bg-red-500' : ''
-							)}
-							onClick={() => {
-								fetcher.submit({
-									// eslint-disable-next-line @typescript-eslint/naming-convention
-									'dns_config.magic_dns': !isEnabled
-								}, {
-									method: 'PATCH',
-									encType: 'application/json'
-								})
-
-								setIsOpen(false)
-							}}
-						>
-							{isEnabled ? 'Disable' : 'Enable'}
-						</Button>
-					</Dialog.Panel>
-				</div>
-			</Dialog>
+			{Modal}
 		</>
 	)
 }
