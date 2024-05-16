d57ee2677c9585430b43c0b72e38fae9715e8880
Aarnav Tale <aarnavtale@icloud.com>
2024-05-15 22:22:38 -0400

    feat: react 19 to take advantage of compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
 import { vitePlugin as remix } from '@remix-run/dev'
 import { installGlobals } from '@remix-run/node'
 import { defineConfig } from 'vite'
+import babel from 'vite-plugin-babel'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 installGlobals()
 
+const ReactCompilerConfig = {}
+
 export default defineConfig({
 	base: '/admin/',
-	plugins: [remix({
-		basename: '/admin/'
-	}), tsconfigPaths()]
+	plugins: [
+		remix({
+			basename: '/admin/',
+		}),
+		tsconfigPaths(),
+		babel({
+			filter: /\.[jt]sx?$/,
+			babelConfig: {
+				presets: ['@babel/preset-typescript'], // if you use TypeScript
+				plugins: [
+					['babel-plugin-react-compiler', ReactCompilerConfig],
+				],
+			},
+		}),
+	],
 })
