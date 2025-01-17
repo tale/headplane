de9a938da22a798f70722574aaa13b7451e886dd
Aarnav Tale <aarnavtale@icloud.com>
2025-01-17 11:46:36 +0000

    chore: use healthcheck method in healthz

--- app/routes/util/healthz.ts
+++ app/routes/util/healthz.ts
@@ -1,26 +1,12 @@
-import { loadContext } from '~/utils/config/headplane';
-import { HeadscaleError, pull } from '~/utils/headscale';
+import { healthcheck } from '~/utils/headscale';
 import log from '~/utils/log';
 
 export async function loader() {
-	const context = await loadContext();
-	const prefix = context.headscaleUrl;
-	const health = new URL('health', prefix);
-	log.debug('APIC', 'GET %s', health.toString());
-	let healthy = false
-
+	let healthy = false;
 	try {
-		const res = await fetch(health.toString(), {
-			headers: {
-				'Accept': 'application/json',
-			},
-		});
-
-		if (res.status === 200) {
-			healthy = true;
-		}
-	} catch (e) {
-		log.debug('APIC', 'GET %s failed with error %s', health.toString(), e);
+		healthy = await healthcheck();
+	} catch (error) {
+		log.debug('APIC', 'Healthcheck failed %o', error);
 	}
 
 	return new Response(JSON.stringify({ status: healthy ? 'OK' : 'ERROR' }), {
