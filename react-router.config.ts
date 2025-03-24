b8d22beb170087d7e8b6f4bb1afd9d6dd7dd4ea9
Aarnav Tale <aarnavtale@icloud.com>
2025-03-24 16:08:34 -0400

    feat: bundle node_modules into the server

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,4 +3,8 @@ import type { Config } from '@react-router/dev/config';
 export default {
 	basename: '/admin/',
 	ssr: true,
+	future: {
+		unstable_optimizeDeps: true,
+		unstable_splitRouteModules: 'enforce',
+	},
 } satisfies Config;
