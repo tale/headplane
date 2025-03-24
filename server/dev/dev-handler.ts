3710c6271b9f19a519d675e2f8e5c0e87fc8bd10
Aarnav Tale <aarnavtale@icloud.com>
2025-03-24 13:57:47 -0400

    chore: remove old server code

--- server/dev/dev-handler.ts
+++
@@ -1,7 +0,0 @@
-import { createRequestHandler } from 'react-router';
-
-export default createRequestHandler(
-	// @ts-expect-error: React Router Vite plugin
-	() => import('virtual:react-router/server-build'),
-	'development',
-);
