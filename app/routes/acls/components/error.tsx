43e96e11f4162fbafd1051d0c304fb7e75018855
Aarnav Tale <aarnavtale@icloud.com>
2025-04-03 03:00:11 -0400

    feat: support acl capabilities check

--- app/routes/acls/components/error.tsx
+++ app/routes/acls/components/error.tsx
@@ -1,24 +1,44 @@
 import { AlertIcon } from '@primer/octicons-react';
-import cn from '~/utils/cn';
-
+import React from 'react';
 import Card from '~/components/Card';
-import Code from '~/components/Code';
 
-interface Props {
-	message: string;
+interface NoticeViewProps {
+	title: string;
+	children: React.ReactNode;
 }
 
-export function ErrorView({ message }: Props) {
+export function NoticeView({ children, title }: NoticeViewProps) {
 	return (
-		<Card variant="flat" className="max-w-full mb-4">
+		<Card variant="flat" className="max-w-2xl my-8">
 			<div className="flex items-center justify-between">
-				<Card.Title className="text-xl mb-0">Error</Card.Title>
+				<Card.Title className="text-xl mb-0">{title}</Card.Title>
+				<AlertIcon className="w-8 h-8 text-yellow-500" />
+			</div>
+			<Card.Text className="mt-4">{children}</Card.Text>
+		</Card>
+	);
+}
+
+interface ErrorViewProps {
+	children: string;
+}
+
+export function ErrorView({ children }: ErrorViewProps) {
+	const [title, ...rest] = children.split(':');
+	const formattedMessage = rest.length > 0 ? rest.join(':').trim() : children;
+
+	return (
+		<Card variant="flat" className="max-w-2xl mb-4">
+			<div className="flex items-center justify-between">
+				<Card.Title className="text-xl mb-0">
+					{title.trim() ?? 'Error'}
+				</Card.Title>
 				<AlertIcon className="w-8 h-8 text-red-500" />
 			</div>
 			<Card.Text className="mt-4">
-				Could not apply changes to your ACL policy due to the following error:
+				Could not apply changes to the ACL policy:
 				<br />
-				<Code>{message}</Code>
+				<span className="font-mono">{formattedMessage}</span>
 			</Card.Text>
 		</Card>
 	);
