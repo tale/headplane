4fedf4d0429b6ce5d4f36f3fe20513d676b7f1c7
Aarnav Tale <aarnavtale@icloud.com>
2025-04-02 20:09:10 -0400

    fix: only use basename in ssr build

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ if (!version) {
 }
 
 export default defineConfig(({ isSsrBuild }) => ({
-	base: `${prefix}/`,
+	base: isSsrBuild ? `${prefix}/` : undefined,
 	plugins: [reactRouterHonoServer(), reactRouter(), tsconfigPaths()],
 	css: {
 		postcss: {
