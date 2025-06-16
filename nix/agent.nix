799d7b5728d5d5618c6ad078a7dea52e8d898fa4
Aarnav Tale <aarnavtale@icloud.com>
2025-06-16 11:41:04 -0400

    chore: update go deps nix hash

--- nix/agent.nix
+++ nix/agent.nix
@@ -3,7 +3,7 @@ buildGoModule {
   pname = "hp_agent";
   version = (builtins.fromJSON (builtins.readFile ../package.json)).version;
   src = ../.;
-  vendorHash = "sha256-qAB6Q19ymeom7126N7ASaFkcqDBRdbjgmG/OTpcX9sI=";
+  vendorHash = "sha256-3hZzDORAH+D4FW6SkOv3Enddd+q36ZALryvCPD9E5Ac=";
   ldflags = ["-s" "-w"];
   env.CGO_ENABLED = 0;
 }
