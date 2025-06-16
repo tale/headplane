b34a3f0ca1e11b68602b094e6c27684222f0ec35
Aarnav Tale <aarnavtale@icloud.com>
2025-06-16 11:43:29 -0400

    chore: sigh update pnpm deps hash

--- nix/package.nix
+++ nix/package.nix
@@ -23,7 +23,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   pnpmDeps = pnpm_10.fetchDeps {
     inherit (finalAttrs) pname version src;
-    hash = "sha256-xjjkqbgjYaAGYAmlTFE+Lq3Hp6myZKaW3br0YTDNhQA=";
+    hash = "sha256-owncSijanuu+So2LZqAy3XEz12hfTgcjCd3cVsgT4a8=";
   };
 
   buildPhase = ''
