fdbfe584c5a4ea88a675736d4fdc1e8fcb0526c5
Igor Ramazanov <igor.ramazanov@magine.com>
2025-07-29 15:22:28 +0000

    feat: nix: add a new `mise` task and nix flake output to generate NixOS docs
    Adds a new Nix flake output `headplane-nixos-docs` which generates a
    Markdown page with all NixOS `services.headplane.settings.*` options.
    
    Replaces existing handwritten options documentation.

--- flake.nix
+++ flake.nix
@@ -31,6 +31,7 @@ rec {
       packages = {
         headplane = pkgs.callPackage ./nix/package.nix {headplane-ssh-wasm = packages.headplane-ssh-wasm;};
         headplane-agent = pkgs.callPackage ./nix/agent.nix {};
+        headplane-nixos-docs = pkgs.callPackage ./nix/docs.nix {};
         headplane-ssh-wasm = pkgs.callPackage ./nix/ssh-wasm.nix {};
       };
       checks.default = pkgs.symlinkJoin {
@@ -64,6 +65,7 @@ rec {
       overlays.default = final: prev: {
         headplane = final.callPackage ./nix/package.nix {headplane-ssh-wasm = final.headplane-ssh-wasm;};
         headplane-agent = final.callPackage ./nix/agent.nix {};
+        headplane-nixos-docs = final.callPackage ./nix/docs.nix {};
         headplane-ssh-wasm = final.callPackage ./nix/ssh-wasm.nix {};
       };
       nixosModules.headplane = import ./nix/module.nix;
