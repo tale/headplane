361bc493c05c23acb2c763dc29fea2a2dbe311b4
Aarnav Tale <aarnavtale@icloud.com>
2025-08-18 13:28:16 -0400

    chore: pin vitepress to 2.0.0 and add sponsor link

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,42 +1,42 @@
-import { defineConfig } from "vitepress";
+import { defineConfig } from 'vitepress';
 
-// https://vitepress.dev/reference/site-config
 export default defineConfig({
-	title: "Headplane",
-	description: "Documentation",
-	// base: "https://headplane.net",
-	ignoreDeadLinks: ["/docs/Integrated-Mode", "/docs/Simple-Mode"],
+	title: 'Headplane',
+	description: 'The missing dashboard for Headscale',
+	ignoreDeadLinks: ['/docs/Integrated-Mode', '/docs/Simple-Mode'],
+	cleanUrls: true,
 	themeConfig: {
-		// https://vitepress.dev/reference/default-theme-config
-		nav: [{ text: "Home", link: "/" }],
-
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: 'Sponsor Headplane', link: 'https://github.com/sponsors/tale' },
+		],
 		sidebar: [
 			{
-				text: "Chapters",
+				text: 'Chapters',
 				items: [
-					{ text: "Getting Started", link: "/README" },
-					{ text: "Configuration", link: "/Configuration" },
-					{ text: "Bare-Metal Mode", link: "/Bare-Metal" },
-					{ text: "Integrated Mode", link: "/Integrated-Mode" },
-					{ text: "Simple Mode", link: "/Simple-Mode" },
-					{ text: "Nix", link: "/Nix" },
-					{ text: "NixOS", link: "/NixOS-options" },
-					{ text: "Security", link: "/SECURITY" },
-					{ text: "Contributing", link: "/CONTRIBUTING" },
-					{ text: "Changelog", link: "/CHANGELOG" },
+					{ text: 'Getting Started', link: '/README' },
+					{ text: 'Configuration', link: '/Configuration' },
+					{ text: 'Bare-Metal Mode', link: '/Bare-Metal' },
+					{ text: 'Integrated Mode', link: '/Integrated-Mode' },
+					{ text: 'Simple Mode', link: '/Simple-Mode' },
+					{ text: 'Nix', link: '/Nix' },
+					{ text: 'NixOS', link: '/NixOS-options' },
+					{ text: 'Security', link: '/SECURITY' },
+					{ text: 'Contributing', link: '/CONTRIBUTING' },
+					{ text: 'Changelog', link: '/CHANGELOG' },
 				],
 			},
 		],
 
 		socialLinks: [
-			{ icon: "github", link: "https://github.com/tale/headplane" },
+			{ icon: 'github', link: 'https://github.com/tale/headplane' },
 		],
 
 		lastUpdated: {
-			text: "Updated at",
+			text: 'Updated at',
 			formatOptions: {
-				dateStyle: "full",
-				timeStyle: "medium",
+				dateStyle: 'full',
+				timeStyle: 'medium',
 			},
 		},
 	},
