98e0806e5a04c43c1c48e311d583e395021f5696
Aarnav Tale <git@tale.me>
2026-04-07 00:32:59 -0400

    fix: handle the tag owner username

--- app/utils/user.ts
+++ app/utils/user.ts
@@ -1,5 +1,9 @@
 import type { User } from "~/types/User";
 
 export function getUserDisplayName(user: User): string {
+  if (user.name === "tagged-devices") {
+    return "Tag-owned";
+  }
+
   return user.name || user.displayName || user.email || user.id;
 }
