b16ee7cc7ed84f24f0b41fd6e61159c42ae41842
Sandro <sandro.jaeckel@gmail.com>
2026-07-08 00:05:24 +0200

    Fix wasm ssh in nix build

--- nix/package.nix
+++ nix/package.nix
@@ -38,9 +38,9 @@ in
 
     buildPhase = ''
       runHook preBuild
-      cp ${headplane-ssh-wasm}/hp_ssh.wasm app/hp_ssh.wasm
-      cp ${headplane-ssh-wasm}/wasm_exec.js app/wasm_exec.js
-      pnpm react-router build
+      cp ${headplane-ssh-wasm}/hp_ssh.wasm public/hp_ssh.wasm
+      cp ${headplane-ssh-wasm}/wasm_exec.js public/wasm_exec.js
+      pnpm build
       runHook postBuild
     '';
 
@@ -49,7 +49,6 @@ in
       mkdir -p $out/{bin,share/headplane}
       cp -r build $out/share/headplane/
       cp -r drizzle $out/share/headplane/
-      sed -i "s;$PWD;../..;" $out/share/headplane/build/server/index.js
       makeWrapper ${lib.getExe nodejs_24} $out/bin/headplane \
         --chdir $out/share/headplane \
         --add-flags $out/share/headplane/build/server/index.js
